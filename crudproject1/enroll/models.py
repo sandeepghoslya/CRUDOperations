c4ddedf998bb7baa1841826eb260691fb60dea16
sandeepghoslya <sandeepghosalya123@gmail.com>
2020-11-06 02:50:06 +0530

    crud operaton project

---
+++ crudproject1/enroll/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class User(models.Model):
+    objects = None
+    name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
